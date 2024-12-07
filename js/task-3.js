3bc22ce20af0fe6a3f1bc9b52998047d88fffaae
SK <v.buh911@gmail.com>
2024-12-07 11:09:43 +0200

    fixed

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function checkForSpam (message) {
    message = message.toLowerCase();
-    if (message.includes('spam', 'sale')) {
+    if (message.includes('spam') || message.includes('sale')) {
         return true;       
     } else {return false;}
 }
